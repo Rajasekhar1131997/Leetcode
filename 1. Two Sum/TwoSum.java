310ad2a153b234cf8079dd883b0af8cea0130f64
Rajasekhar Reddy Kolagotla <rajasekhar1131997@gmail.com>
2025-05-10 23:28:47 +0000

    Two Sum problem

--- 1. Two Sum/TwoSum.java
+++ 1. Two Sum/TwoSum.java
@@ -1,3 +1,6 @@
+// LeetCode 1. Two Sum
+// https://leetcode.com/problems/two-sum/
+// Below Approach is using Brute Force
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for (int i=0;i<nums.length;i++){
@@ -5,6 +8,23 @@ class Solution {
                 if (nums[i] + nums[j] == target){
                     return new int[]{i, j};
                 }
+            }
+        }
+        return new int[]{};
+    }
+}
+
+// Approach 2: Using HashMap
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> mp = new HashMap<>();
+        for (int i=0;i<nums.length;i++){
+            mp.put(nums[i], i);
+        }
+        for (int i=0;i<nums.length;i++){
+            int complement = target - nums[i];
+            if (mp.containsKey(complement) && mp.get(complement) != i){
+                return new int[]{i, mp.get(complement)};
             }
         }
         return new int[]{};
