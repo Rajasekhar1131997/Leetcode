2b79053fa478dc11b0630106d0d1123da18855dc
Rajasekhar Reddy Kolagotla <rajasekhar1131997@gmail.com>
2025-08-29 06:50:11 +0000

    Leetcode Problems

---
+++ 195-Tenth Line.sh
@@ -0,0 +1,5 @@
+# Leetcode Problem 735: Tenth Line
+# https://leetcode.com/problems/tenth-line/description/
+
+# Read from the file file.txt and output the tenth line to stdout.
+sed -n '10p' file.txt
